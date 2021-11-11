ae9a40785969e6eaf0ec9a23298f827859cc613f
BHUVAN CHANDRA SARAKAM <s544899@nwmissouri.edu>
2021-11-10 23:02:25 -0600

    controllers/winter.js

--- controllers/winter.js
+++ controllers/winter.js
@@ -1,20 +1,20 @@
-var winter = require('../models/winter');
+import winter, { find } from '../models/winter';
 // List of all Winter
-exports.winter_list = function (req, res) {
+export function winter_list (req, res) {
     res.send('NOT IMPLEMENTED: Winter list');
-};
+}
 // for a specific Winter.
-exports.winter_detail = function (req, res) {
+export function winter_detail (req, res) {
     res.send('NOT IMPLEMENTED: Winter detail: ' + req.params.id);
-};
+}
 // Handle Winter create on POST.
-exports.winter_create_post = async function (req, res) {
+export async function winter_create_post (req, res) {
     console.log(req.body)
     let document = new winter();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"gas_type":"regular", "quantity":13, "cost":43.56}
+    // {"wTemp":65, "wArea":"Kansas City", "wMonth":"Oct"}
     document.wTemp = req.body.wTemp;
     document.wArea = req.body.wArea;
     document.wMonth = req.body.wMonth;
@@ -25,18 +25,18 @@ exports.winter_create_post = async function (req, res) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
-};
+}
 // Handle Winter delete form on DELETE.
-exports.winter_delete = function (req, res) {
+export function winter_delete (req, res) {
     res.send('NOT IMPLEMENTED: Winter delete DELETE ' + req.params.id);
-};
+}
 // Handle Winter update form on PUT.
-exports.winter_update_put = function (req, res) {
+export function winter_update_put (req, res) {
     res.send('NOT IMPLEMENTED: Winter update PUT' + req.params.id);
-};
+}
 
 // List of all Winter
-exports.winter_list = async function (req, res) {
+export async function winter_list (req, res) {
     try {
         theWinter = await winter.find();
         res.send(theWinter);
@@ -44,13 +44,13 @@ exports.winter_list = async function (req, res) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
-};
+}
 
 // VIEWS
 // Handle a show all view
-exports.winter_view_all_Page = async function (req, res) {
+export async function winter_view_all_Page (req, res) {
     try {
-        theWinter = await winter.find();
+        theWinter = await find();
         res.render('winter', {
             title: 'Winter Search Results',
             results: theWinter
@@ -59,4 +59,4 @@ exports.winter_view_all_Page = async function (req, res) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
-};
+}
