f59db9c9c8a4d90bbd8e43c9c04cd10e344386d9
BHUVAN CHANDRA SARAKAM <s544899@nwmissouri.edu>
2021-11-17 19:10:42 -0600

    added winter.js

--- routes/winter.js
+++ routes/winter.js
@@ -3,15 +3,15 @@ const winter_controllers= require('../controllers/winter');
 var router = express.Router();
 
 /* GET winter */
-router.get('/', winter_controlers.winter_view_all_Page );
+router.get('/', winter_controllers.winter_view_all_Page );
 /* GET detail winter page */
-router.get('/detail', winter_controlers.winter_view_one_Page);
+router.get('/detail', winter_controllers.winter_view_one_Page);
 /* GET create winter page */
-router.get('/create', winter_controlers.winter_create_Page);
+router.get('/create', winter_controllers.winter_create_Page);
 /* GET update winter page */
-router.get('/update', winter_controlers.winter_update_Page);
+router.get('/update', winter_controllers.winter_update_Page);
 /* GET delete winter page */
-router.get('/delete', winter_controlers.winter_delete_Page);
+router.get('/delete', winter_controllers.winter_delete_Page);
 
 module.exports = router;
 
