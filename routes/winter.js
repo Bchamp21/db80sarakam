2feb87ef7b0e665631dd7e22a1774b1f0363230a
BHUVAN CHANDRA SARAKAM <s544899@nwmissouri.edu>
2021-11-17 19:34:08 -0600

    detail

--- routes/winter.js
+++ routes/winter.js
@@ -7,11 +7,11 @@ router.get('/', winter_controllers.winter_view_all_Page );
 /* GET detail winter page */
 router.get('/detail', winter_controllers.winter_view_one_Page);
 /* GET create winter page */
-router.get('/create', winter_controllers.winter_create_Page);
-/* GET update winter page */
-router.get('/update', winter_controllers.winter_update_Page);
-/* GET delete winter page */
-router.get('/delete', winter_controllers.winter_delete_Page);
+// router.get('/create', winter_controllers.winter_create_Page);
+// /* GET update winter page */
+// router.get('/update', winter_controllers.winter_update_Page);
+// /* GET delete winter page */
+// router.get('/delete', winter_controllers.winter_delete_Page);
 
 module.exports = router;
 
