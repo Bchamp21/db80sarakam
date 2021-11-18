d8c4fc05e3439129b9efd7cdac746fd1ab8d0b8b
BHUVAN CHANDRA SARAKAM <s544899@nwmissouri.edu>
2021-11-17 18:03:18 -0600

    Added winter.js

--- controllers/winter.js
+++ controllers/winter.js
@@ -26,10 +26,20 @@ exports.winter_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
-// Handle Winter delete form on DELETE.
-exports.winter_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Winter delete DELETE ' + req.params.id);
+
+// Handle Winter delete on DELETE.
+exports.winter_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await winter.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
+
 // Handle Winter update form on PUT.
 exports.winter_update_put = function (req, res) {
     res.send('NOT IMPLEMENTED: Winter update PUT' + req.params.id);
@@ -59,4 +69,37 @@ exports.winter_view_all_Page = async function (req, res) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
+};
+
+
+// for a specific Costume.
+exports.winter_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await winter.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+// Handle Winter update form on PUT.
+exports.winter_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await winter.findById( req.params.id)
+// Do updates of properties
+if(req.body.wTemp) toUpdate.wTemp = req.body.wTemp;
+if(req.body.wArea) toUpdate.wArea = req.body.wArea;
+if(req.body.wMonth) toUpdate.wMonth = req.body.wMonth;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
--- routes/winter.js
+++ routes/winter.js
@@ -3,4 +3,6 @@ const winter_controllers= require('../controllers/winter');
 var router = express.Router();
 /* GET costumes */
 router.get('/', winter_controllers.winter_view_all_Page );
+router.get('/winter/:id', winter_controllers.winter_detail);
 module.exports = router;
+
